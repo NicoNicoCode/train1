e8900e99f95f67aa84ed55cb0ca815b483e3fae2
root <root@localhost.localdomain>
2017-11-15 02:53:13 -0500

    changed the exam2.sh

--- exam2.sh
+++ exam2.sh
@@ -1,65 +1,197 @@
-#!/bin/bash
-#check if you have the mysql
-if [`rpm -qa | grep mysql` -ne 0]
+utor shenyuanyu
+
+#安装编译工具及库文件
+yumware=(gcc gcc-c++ make autoconf libtool-ltdl-devel gd-devel freetype-devel libxml2-devel libjpeg-devel libpng-devel openssl-devel curl-devel bison patch unzip ncurses-devel sudo bzip2 flex libaio-devel wget
+	)
+for t in ${yumware[@]}
+do
+	echo "start to install ${t}"
+	yum -y install ${t}
+	if test $? -eq 0
+	then 
+		echo "Good Luck,${t} install success"
+	else
+		echo "Unfortunatly,${t} install fail"
+		exit
+	fi
+done
+
+#安装cmake编译器
+echo "start install cmake tool"
+#下载压缩包
+wget https://cmake.org/files/v3.9/cmake-3.9.6.tar.gz
+#解压压缩包
+tar zxvf cmake-3.9.6.tar.gz
+#进入安装目录 
+cd cmake-3.9.6
+#编译安装
+./bootstrap
+make && make install
+#返回用户根目录
+cd
+echo "cmake tool install end"
+
+#安装mysql
+echo "start install mysql"
+#下载安装包
+wget http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.16.tar.gz
+#解压安装包
+tar zxvf mysql-5.6.16.tar.gz
+#进入安装包目录
+cd mysql-5.6.16
+#编译安装
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local/mysql/ \
+-DMYSQL_UNIX_ADDR=/tmp/mysql.sock -DDEFAULT_CHARSET=utf8 \
+-DDEFAULT_COLLATION=utf8_general_ci \
+-DMYSQL_MAINTAINER_MODE=OFF \
+-DMYSQL_DATADIR=/usr/local/mysql/data \
+-DMYSQL_TCP_PORT=3306
+#make命令
+make && make install
+echo "mysql install end"
+
+#mysql配置
+echo "start set mysql config"
+#创建mysql运行使用的用户组mysql
+/usr/sbin/groupadd mysql
+/usr/sbin/useradd -g mysql mysql
+#创建binlog和库的存储路径并赋予mysql用户权限
+mkdir -p /usr/local/mysql/binlog /www/data_mysql
+chown mysql.mysql /usr/local/mysql/binlog/ /www/data_mysql/
+
+#将配置文件/etc/my.cnf替换为下面内容
+cnf="[client]
+port = 3306
+socket = /tmp/mysql.sock
+[mysqld]
+replicate-ignore-db = mysql
+replicate-ignore-db = test
+replicate-ignore-db = information_schema
+user = mysql
+port = 3306
+socket = /tmp/mysql.sock
+basedir = /usr/local/mysql
+datadir = /www/data_mysql
+log-error = /usr/local/mysql/mysql_error.log
+pid-file = /usr/local/mysql/mysql.pid
+open_files_limit = 65535
+back_log = 600
+max_connections = 5000
+max_connect_errors = 1000
+table_open_cache = 1024
+external-locking = FALSE
+max_allowed_packet = 32M
+sort_buffer_size = 1M
+join_buffer_size = 1M
+thread_cache_size = 600
+#thread_concurrency = 8
+query_cache_size = 128M
+query_cache_limit = 2M
+query_cache_min_res_unit = 2k
+default-storage-engine = MyISAM
+default-tmp-storage-engine=MYISAM
+thread_stack = 192K
+transaction_isolation = READ-COMMITTED
+tmp_table_size = 128M
+max_heap_table_size = 128M
+log-slave-updates
+log-bin = /usr/local/mysql/binlog/binlog
+binlog-do-db=oa_fb
+binlog-ignore-db=mysql
+binlog_cache_size = 4M
+binlog_format = MIXED
+max_binlog_cache_size = 8M
+max_binlog_size = 1G
+relay-log-index = /usr/local/mysql/relaylog/relaylog
+relay-log-info-file = /usr/local/mysql/relaylog/relaylog
+relay-log = /usr/local/mysql/relaylog/relaylog
+expire_logs_days = 10
+key_buffer_size = 256M
+read_buffer_size = 1M
+read_rnd_buffer_size = 16M
+bulk_insert_buffer_size = 64M
+myisam_sort_buffer_size = 128M
+myisam_max_sort_file_size = 10G
+myisam_repair_threads = 1
+myisam_recover
+interactive_timeout = 120
+wait_timeout = 120
+skip-name-resolve
+#master-connect-retry = 10
+slave-skip-errors = 1032,1062,126,1114,1146,1048,1396
+server-id = 1
+loose-innodb-trx=0 
+loose-innodb-locks=0 
+loose-innodb-lock-waits=0 
+loose-innodb-cmp=0 
+loose-innodb-cmp-per-index=0
+loose-innodb-cmp-per-index-reset=0
+loose-innodb-cmp-reset=0 
+loose-innodb-cmpmem=0 
+loose-innodb-cmpmem-reset=0 
+loose-innodb-buffer-page=0 
+loose-innodb-buffer-page-lru=0 
+loose-innodb-buffer-pool-stats=0 
+loose-innodb-metrics=0 
+loose-innodb-ft-default-stopword=0 
+loose-innodb-ft-inserted=0 
+loose-innodb-ft-deleted=0 
+loose-innodb-ft-being-deleted=0 
+loose-innodb-ft-config=0 
+loose-innodb-ft-index-cache=0 
+loose-innodb-ft-index-table=0 
+loose-innodb-sys-tables=0 
+loose-innodb-sys-tablestats=0 
+loose-innodb-sys-indexes=0 
+loose-innodb-sys-columns=0 
+loose-innodb-sys-fields=0 
+loose-innodb-sys-foreign=0 
+loose-innodb-sys-foreign-cols=0
+slow_query_log_file=/usr/local/mysql/mysql_slow.log
+long_query_time = 1
+[mysqldump]
+quick
+max_allowed_packet = 32M"
+
+#将替换的内容写入到/etc/my.cnf
+echo "${cnf}" > /etc/my.cnf
+
+
+#使用mysql自带脚本初始化数据库
+/usr/local/mysql/scripts/mysql_install_db --defaults-file=/etc/my.cnf --basedir=/usr/local/mysql --user=mysql
+
+#创建开机启动脚本
+cd /usr/local/mysql/
+#赋予目录权限
+chown -R mysql:mysql ./
+cp support-files/mysql.server /etc/rc.d/init.d/mysqld 
+chkconfig --add mysqld 
+chkconfig --level 35 mysqld on
+
+#启动mysql服务器
+service mysqld start
+
+#将mysql设置入环境变量，方便使用
+echo 'export PATH=${PATH}:/usr/local/mysql/bin' >> /etc/profile
+#激活加入的环境变量
+source /etc/profile
+if test $? -eq 0
 then
-    echo -e "You have download the mysql!\n"
+	echo "mysql环境变量设置成功"
 else
-    echo -e "You don't have download the mysql!\n"
-    wget https://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.38.tar.gz
-    echo -e "I have download the sourcode of mysql!\n"
-    #compile the sourcode of mysql
-    tar -zxvf mysql-5.6.38.tar.gz -C /usr/local
-    groupadd mysql
-    useradd -r -g mysql mysql
-    cd /usr/local/mysql-5.6.38
-    #option of conpile the mysql
-    echo -e "compiling..."
-    cmake \
-    -DCMAKE_INSTALL_PREFIX=/usr/local/mysql \
-    -DMYSQL_DATADIR=/usr/local/mysql/data \
-    -DWITH_INNOBASE_STORAGE_ENGINE=1 \
-    -DMYSQL_UNIX_ADDR=/usr/local/mysql/mysql.sock \
-    -DMYSQL_UNIX_USER=mysql \
-    -DDEFAULT_CHARSET=utf8 \
-    -DDEFAULT_COLLATION=utf8_general_ci
-
-    #start the compile
-    #multiple kernel to compile
-
-    make  
-    make install
-    make clean
-    
-    chown -Rf mysql:mysql /usr/local/mysql
-    chown -Rf mysql:mysql /usr/local/mysql/data
-    
-    cd /usr/local/mysql
-    #make a copy
-    cp /usr/local/mysql/support-files/mysql.server /etc/init.d/mysqld
-
-    #give the directory to priority
-    chmod +x /etc/init.d/mysqld
-    chkconfig --add mysqld
-    chkconfig mysqld on
-    
-    #setup the mysql
-    scripts/mysql_install_db --user=mysql --basedir=/usr/local/mysql\ 
-    --datadir=/home/data/mysql/data\
-    --defaultdir=/usr/local/mysql/my.cnf
-    
-    #start the service
-    cd /usr/local/mysql/bin
-    service mysql start
-   
-    echo -e "Good Luck!You have installed the mysql"
- 
-    echo -e "Your password is 123456"
-    mysqladmin -u root password 123456 
-
-    #login to mysql used by root   
-    mysql -u root -p 123456
-
-    create database if not exists myapp
-    show database
+	echo "mysql环境变量设置失败"
+	exit
 fi
 
+#连接 MySQL，并创建myapp数据库
+mysql << EOF
+create database myapp
+EOF
+
+if test $? -eq 0
+then
+	echo "create myapp database success"
+else
+	echo "create myapp database fail"
+	exit
+fi
